c12b66eddeccafac456afcc041855609b1857010
Indunil1992 <indunil@adroitlogic.com>
2019-06-21 13:13:38 +0530

    cmnt iii

--- Sample1/lambda.js
+++ Sample1/lambda.js
@@ -1,19 +1,32 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed jsb"});
-s3.getBucketLocation({
-    'Bucket': "sigma-slappforge-318300609668-us-east-1-build-artifacts-922355"
+s3.getObject({
+    'Bucket': "aws-amplify-hotspaces",
+    'Key': "'h"
 }).promise()
     .then(data => {
-        console.log(data);           // successful response
+        console.log(data); 
+        callback(null, {"message": "Successfull : ",data});          // successful response
         /*
         data = {
-            LocationConstraint: "us-west-2"
+            AcceptRanges: "bytes", 
+            ContentLength: 3191, 
+            ContentType: "image/jpeg", 
+            ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+            LastModified: "<Date Representation>", 
+            Metadata: {}, 
+            TagCount: 2, 
+            VersionId: "null"
         }
         */
     })
     .catch(err => {
-        console.log(err, err.stack); // an error occurred
+ 
+        console.log(err, err.stack); 
+        callback(null, {"message": "error :",err});// an error occurred
     });
+
+
+    
+    }
