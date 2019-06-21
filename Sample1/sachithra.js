0037da86b16ee11af2dd37e7b0bebb7f17554bc8
Indunil1992 <indunil@adroitlogic.com>
2019-06-21 13:57:57 +0530

    sachithraaa

--- Sample1/sachithra.js
+++ Sample1/sachithra.js
@@ -1,4 +1,24 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
+
+    callback(null, { "message": "Successfully executed" });
+    s3.deleteObject({
+        'Bucket': "aws-amplify-hotspaces",
+        'Key': "hello world"
+    }).promise()
+        .then(data => {
+            console.log(data);      
+            callback(null, { "message": "Successfull : ", data }); 
+                 // successful response
+            /*
+                data = {}
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack);
+            callback(null, { "message": "error :", err });
+             // an error occurred
+        });
 }
