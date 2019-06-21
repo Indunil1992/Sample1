be552e7b24600c3e5ebb9d55df98971c85d1523c
Indunil1992 <indunil@adroitlogic.com>
2019-06-21 11:54:49 +0530

    jst

--- Sample1/lambda.js
+++ Sample1/lambda.js
@@ -1,4 +1,19 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+
+    callback(null, { "message": "Successfully executed" });
+    sns.setTopicAttributes({
+        AttributeName: 'DisplayName',
+        AttributeValue: 'indunil',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+        callback(null, {"message": "Successfully executed"});
 }
