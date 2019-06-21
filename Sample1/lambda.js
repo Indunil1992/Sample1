1fb95d2f3a27ed25dd8af7029fc03aa5cc4d3b99
Indunil1992 <indunil@adroitlogic.com>
2019-06-21 11:25:41 +0530

    kkkkkkk

--- Sample1/lambda.js
+++ Sample1/lambda.js
@@ -5,14 +5,23 @@ const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
     console.log("check 1 ");
     sns.getTopicAttributes({
+        sns.getTopicAttributes({
+            TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+        }).promise()
+            .then(data => {
+                // your code goes here
+            })
+            .catch(err => {
+                // error handling goes here
+            });
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
         .then(data => {
             // your code goes here
-             console.log("check 1=2 ");
+            console.log("check 1=2 ");
         })
         .catch(err => {
-             console.log("check 4");
+            console.log("check 4");
             // error handling goes here
         });
 
