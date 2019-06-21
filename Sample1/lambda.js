a3277d273742534992d09d9f808314aa7bc7dc41
Indunil1992 <indunil@adroitlogic.com>
2019-06-21 13:23:14 +0530

    Hello world

--- Sample1/lambda.js
+++ Sample1/lambda.js
@@ -1,32 +1,32 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-exports.handler = function(event, context, callback) {
-s3.getObject({
-    'Bucket': "aws-amplify-hotspaces",
-    'Key': "'h"
-}).promise()
-    .then(data => {
-        console.log(data); 
-        callback(null, {"message": "Successfull : ",data});          // successful response
-        /*
-        data = {
-            AcceptRanges: "bytes", 
-            ContentLength: 3191, 
-            ContentType: "image/jpeg", 
-            ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
-            LastModified: "<Date Representation>", 
-            Metadata: {}, 
-            TagCount: 2, 
-            VersionId: "null"
-        }
-        */
-    })
-    .catch(err => {
- 
-        console.log(err, err.stack); 
-        callback(null, {"message": "error :",err});// an error occurred
-    });
+exports.handler = function (event, context, callback) {
+    s3.getObject({
+        'Bucket': "aws-amplify-hotspaces",
+        'Key': "Hello world"
+    }).promise()
+        .then(data => {
+            console.log(data);
+            callback(null, { "message": "Successfull : ", data });          // successful response
+            /*
+            data = {
+                AcceptRanges: "bytes", 
+                ContentLength: 3191, 
+                ContentType: "image/jpeg", 
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+                LastModified: "<Date Representation>", 
+                Metadata: {}, 
+                TagCount: 2, 
+                VersionId: "null"
+            }
+            */
+        })
+        .catch(err => {
+
+            console.log(err, err.stack);
+            callback(null, { "message": "error :", err });// an error occurred
+        });
 
 
-    
-    }
+
+}
