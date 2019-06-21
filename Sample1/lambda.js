1ddfe6ad43778e9fadebf28f9fc87662ac8a97fa
Indunil1992 <indunil@adroitlogic.com>
2019-06-21 12:57:14 +0530

    cmnt 1

--- Sample1/lambda.js
+++ Sample1/lambda.js
@@ -1,21 +1,19 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-
-exports.handler = function (event, context, callback) {
-
-    callback(null, { "message": "Successfully executed" });
-    sns.setTopicAttributes({
-        AttributeName: 'DisplayName',
-        AttributeValue: 'indunil',
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
-    }).promise()
-        .then(data => {
-            callback(null, { "message": "Successfully y executed" })
-            // your code goes here
-        })
-        .catch(err => {
-            callback(null, { "message": "Successfully n  executed" })
-            // error handling goes here
-        });
-        callback(null, {"message": "Successfully gggg executed"});
-}
+const s3 = new AWS.S3();
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed jsb"});
+s3.getBucketLocation({
+    'Bucket': "sigma-slappforge-318300609668-us-east-1-build-artifacts-922355"
+}).promise()
+    .then(data => {
+        console.log(data);           // successful response
+        /*
+        data = {
+            LocationConstraint: "us-west-2"
+        }
+        */
+    })
+    .catch(err => {
+        console.log(err, err.stack); // an error occurred
+    });
